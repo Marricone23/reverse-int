af19ed3411c6373fb8df65b7286a987c669c2cfe
Marricone23 <68504252+Marricone23@users.noreply.github.com>
2022-03-03 22:22:11 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
-const num = parseFloat(n.toString().split('').reverse().join(''));
-return num;
+  const num = parseFloat(n.toString().split('').reverse().join(''));
+  return num;
 };
-reverse(-538);
+
 
